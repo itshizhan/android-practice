bd4e4e14d480c749655fbc189d86a25785dadbad
leeson8888 <251076355@qq.com>
2017-05-09 01:21:18 +0800

    intent practice

---
+++ code/guolin-firstlinecode/chapter2/ActivityTest/app/src/main/java/com/example/leeson8888/activitytest/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.leeson8888.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_second);
+    }
+}
