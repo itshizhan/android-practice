9c59cb9f0448704fb1355a378ef096d28ef8bfc2
leeson8888 <251076355@qq.com>
2017-05-13 01:09:52 +0800

    活动生命周期更新

---
+++ code/guolin-firstlinecode/chapter2/ActivityLifeCycleTest/app/src/main/java/com/example/leeson8888/activitylifecycletest/NormalActivity.java
@@ -0,0 +1,13 @@
+package com.example.leeson8888.activitylifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.normal_layout);
+    }
+}
