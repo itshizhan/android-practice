038edee48dd50d1cadc6c357ebc201c48c3bfdd0
itshizhan <itshizhan@163.com>
2018-04-23 01:54:45 +0800

    add notes and codes

--- AndroidProgrammingGuide/codes/chapterOne/GeoQuiz/app/src/main/java/com/itshizhan/geoquiz/QuizActivity.java
+++ AndroidProgrammingGuide/codes/chapterOne/GeoQuiz/app/src/main/java/com/itshizhan/geoquiz/QuizActivity.java
@@ -2,12 +2,53 @@ package com.itshizhan.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        initView();
+
+    }
+
+
+    private void initView(){
+        mTrueButton  = (Button)findViewById(R.id.true_button);
+        mFalseButton = this.<Button>findViewById(R.id.false_button);
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                //toast.setGravity(Gravity.center,100,10);
+                toast.setGravity(Gravity.TOP, 0, 150);
+                toast.show();
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
--- AndroidProgrammingGuide/notes/chapter1.md
+++ AndroidProgrammingGuide/notes/chapter1.md
@@ -1,4 +1,4 @@
-#  Android 开发基础
+#  1. Android 开发基础
 
 #### activity 和 layout
 
@@ -7,3 +7,7 @@ activity: 是android SDK 中Activity类的一个实例，负责管理用户与�
 layout: 布局定义一系列用户界面对象，以及他们显示在屏幕上的位置。布局的定义保存在XML文件中。
 
 > 布局的命名规则：颠倒Activity子类名的单词顺序并全部转为小写，然后再单词之间添加下划线。例如，Activity 名称为：`QuizActivity.java`， Layout 名称为：`activity_quiz.xml`
+
+#### 组件
+组件定义在Activity xml文件中，例如RelativeLayout 和TextView 组件。每一个组件都是View类或其子类的一个具体实例。
+
