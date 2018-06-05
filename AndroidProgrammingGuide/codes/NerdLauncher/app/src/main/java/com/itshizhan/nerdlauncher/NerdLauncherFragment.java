f5bf415d02ddca7948dc4c5eacd5fcefc30045a6
itshizhan <itshizhan@163.com>
2018-06-06 01:05:59 +0800

    pls ignore

--- AndroidProgrammingGuide/codes/NerdLauncher/app/src/main/java/com/itshizhan/nerdlauncher/NerdLauncherFragment.java
+++ AndroidProgrammingGuide/codes/NerdLauncher/app/src/main/java/com/itshizhan/nerdlauncher/NerdLauncherFragment.java
@@ -1,21 +1,33 @@
 package com.itshizhan.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public  class NerdLauncherFragment extends Fragment {
+    private static final String TAG = "NerdLauncherFragment";
 
     private RecyclerView mRecyclerView;
 
     public static NerdLauncherFragment newInstance() {
        return  new NerdLauncherFragment();
     }
+
 
 
     // 创建fragment view
@@ -26,7 +38,79 @@ public  class NerdLauncherFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_nerd_launcher,container,false);
         mRecyclerView = (RecyclerView)v.findViewById(R.id.app_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         return v;
     }
+
+    private void setupAdapter(){
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent,0);
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+               PackageManager pm = getActivity().getPackageManager();
+               return String.CASE_INSENSITIVE_ORDER.compare(
+                       a.loadLabel(pm).toString(),
+                       b.loadLabel(pm).toString()
+               );
+
+            }
+        });
+
+        Log.i(TAG,"FOUND " + activities.size()+" activities");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder>{
+
+        private final List<ResolveInfo> mActivities;
+
+        public ActivityAdapter(List<ResolveInfo> activities){
+            mActivities = activities;
+        }
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view
+                    = layoutInflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
+    }
+    private class ActivityHolder extends RecyclerView.ViewHolder{
+
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+
+        private ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView)itemView;
+        }
+
+        public void bindActivity(ResolveInfo resolveInfo){
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName  = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+        }
+    }
+
 
 }
