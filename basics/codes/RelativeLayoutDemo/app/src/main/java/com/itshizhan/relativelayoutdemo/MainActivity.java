ebbfe41060c39b58c2001aa9f4cc44c0e448a5a0
itshizhan <itshizhan@163.com>
2018-06-25 00:43:25 +0800

    pls ignore

--- basics/codes/RelativeLayoutDemo/app/src/main/java/com/itshizhan/relativelayoutdemo/MainActivity.java
+++ basics/codes/RelativeLayoutDemo/app/src/main/java/com/itshizhan/relativelayoutdemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.itshizhan.viewcustomdemo;
+package com.itshizhan.relativelayoutdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.counterview_layout);
+        setContentView(R.layout.activity_main);
     }
 }
