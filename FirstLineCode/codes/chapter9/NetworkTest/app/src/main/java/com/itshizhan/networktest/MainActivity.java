d745d0f88744fe231eba4012148d1ca891d3d7c6
itshizhan <itshizhan@163.com>
2018-08-04 14:23:17 +0800

    chapter9 codes

---
+++ FirstLineCode/codes/chapter9/NetworkTest/app/src/main/java/com/itshizhan/networktest/MainActivity.java
@@ -0,0 +1,104 @@
+package com.itshizhan.networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.send_request_btn);
+        textView = (TextView) findViewById(R.id.text_view);
+        button.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.send_request_btn:
+                //Toast.makeText(MainActivity.this,"request",Toast.LENGTH_SHORT).show();
+                sendRequestWithHttpURLConnection();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        // 开启子线程发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+
+                    InputStream inputStream = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder text = new StringBuilder();
+                    String line ;
+                    while ((line = reader.readLine())!=null){
+                        text.append(line);
+                    }
+                    showResponse(text.toString());
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }finally {
+                    if(reader!=null){
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if(connection!=null){
+                        connection.disconnect();
+                    }
+
+                }
+            }
+        }).start();
+
+    }
+
+    private void showResponse(final String s) {
+        //切换到主线程UI线程更新UI
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //UI操作
+                textView.setText(s);
+            }
+        });
+    }
+
+}
--- FirstLineCode/codes/chapter9/WebviewTest/app/src/main/java/com/itshizhan/webviewtest/MainActivity.java
+++ FirstLineCode/codes/chapter9/WebviewTest/app/src/main/java/com/itshizhan/webviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.itshizhan.webviewtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mWebView = findViewById(R.id.web_view);
-        mWebView.loadUrl("https://www.baidu.com");
+        //支持javascript脚本
+        mWebView.getSettings().getJavaScriptEnabled();
+        //在当前webview 中显示网页，不打开系统浏览器
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("https://www.baidu.com/");
     }
 }
