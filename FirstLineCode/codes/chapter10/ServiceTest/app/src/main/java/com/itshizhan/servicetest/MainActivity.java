e4a750822c34ae90c4e2b561ab01edc7da25bd21
itshizhan <itshizhan@163.com>
2018-08-07 00:00:50 +0800

    pls ignore

--- FirstLineCode/codes/chapter10/ServiceTest/app/src/main/java/com/itshizhan/servicetest/MainActivity.java
+++ FirstLineCode/codes/chapter10/ServiceTest/app/src/main/java/com/itshizhan/servicetest/MainActivity.java
@@ -1,12 +1,31 @@
 package com.itshizhan.servicetest;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            MyService.DownloadBinder downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button startBtn = findViewById(R.id.start_service);
         Button stopBtn = findViewById(R.id.stop_service);
+        Button bindBtn = findViewById(R.id.bind_service);
+        Button unbindBtn = findViewById(R.id.unbind_service);
+
+
         startBtn.setOnClickListener(this);
         stopBtn.setOnClickListener(this);
+        bindBtn.setOnClickListener(this);
+        unbindBtn.setOnClickListener(this);
     }
 
 
@@ -30,6 +55,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this,MyService.class);
                 stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent intent = new Intent(this,MyService.class);
+                bindService(intent,mServiceConnection,BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(mServiceConnection);
+
                 break;
             default:
                 break;
--- FirstLineCode/codes/chapter10/ServiceTest/app/src/main/java/com/itshizhan/servicetest/MyService.java
+++ FirstLineCode/codes/chapter10/ServiceTest/app/src/main/java/com/itshizhan/servicetest/MyService.java
@@ -1,26 +1,43 @@
 package com.itshizhan.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
 
     private static final String TAG = "MyService";
+
+
+
     public MyService() {
         Log.d(TAG,"MyService  constructor");
     }
 
+
+    class DownloadBinder extends Binder{
+        public void startDownload(){
+            Log.d(TAG,"startDownload  executed");
+        }
+        public int getProgress(){
+            Log.d(TAG,"getProgress  executed");
+            return 0;
+        }
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG,"MyService  onBind");
         // TODO: Return the communication channel to the service.
         //throw new UnsupportedOperationException("Not yet implemented");
-        return  null;
+        return  new DownloadBinder();
     }
 
 
@@ -28,6 +45,18 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG,"MyService  onCreate");
+        // 创建前台服务
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this,"default")
+                .setContentTitle("this is service title")
+                .setContentText("This is service content")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pi)
+                .build();
+       startForeground(1,notification);
+
     }
 
     @Override
